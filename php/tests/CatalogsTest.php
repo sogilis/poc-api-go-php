19e2da5b74208389a9ebbe01c32f83ae702a7db6
Benoit DUMAS <benoit@sogilis.com>
2021-06-29 13:56:01 +0200

    php: add test get item

--- php/tests/CatalogsTest.php
+++ php/tests/CatalogsTest.php
@@ -8,10 +8,13 @@ class CatalogsTest extends ApiTestCase {
 
   public function testGetCollection(): void
   {
+    // when
     static::createClient()->request('GET', '/api/catalogs', [
       'headers' => [
           'accept' => 'application/json'
       ]]);
+
+    // then
     $this->assertResponseIsSuccessful();
     $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
     $this->assertJsonContains([
@@ -29,6 +32,29 @@ class CatalogsTest extends ApiTestCase {
         "created_at" => "2021-06-29T06:18:25.860Z",
         "created_by" => "Benoit"
       ]
+    ]);
+  }
+
+  public function testGetItem(): void
+  {
+    // given
+    $id = "7357f822-1667-5191-8a49-7959de2b98c8";
+
+    // when
+    static::createClient()->request('GET', '/api/catalogs/' . $id, [
+      'headers' => [
+          'accept' => 'application/json'
+      ]]);
+
+    // then
+    $this->assertResponseIsSuccessful();
+    $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
+    $this->assertJsonContains([
+      "id" => "7357f822-1667-5191-8a49-7959de2b98c8",
+      "supplier_name" => "Decathlon",
+      "enabled" => true,
+      "created_at" => "2021-06-29T06:18:25.860Z",
+      "created_by" => "Benoit"
     ]);
   }
 
